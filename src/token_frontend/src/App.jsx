fb883c25766a6958e1834dfd8bd7378423e3a1cb
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-06-09 09:48:53 -0400

    Set up starter project with correct files.

--- src/token_frontend/src/App.jsx
+++ src/token_frontend/src/App.jsx
@@ -1,31 +1,46 @@
-import { useState } from 'react';
-import { token_backend } from 'declarations/token_backend';
+import { useState } from "react";
+import { token_backend } from "declarations/token_backend";
+
+// Components
+import Header from "./components/Header";
+import Faucet from "./components/Faucet";
+import Balance from "./components/Balance";
+import Transfer from "./components/Transfer";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    token_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div id="screen">
+      <Header />
+      <Faucet />
+      <Balance />
+      <Transfer />
+    </div>
   );
+  // Deafault React App
+  // const [greeting, setGreeting] = useState('');
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   token_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
+
+  // return (
+  //   <main>
+  //     <img src="/logo2.svg" alt="DFINITY logo" />
+  //     <br />
+  //     <br />
+  //     <form action="#" onSubmit={handleSubmit}>
+  //       <label htmlFor="name">Enter your name: &nbsp;</label>
+  //       <input id="name" alt="Name" type="text" />
+  //       <button type="submit">Click Me!</button>
+  //     </form>
+  //     <section id="greeting">{greeting}</section>
+  //   </main>
+  // );
 }
 
 export default App;
