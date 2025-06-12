6484e921aa43642ce031e1e5cffa37c8e621bc40
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-06-12 15:17:59 -0400

    Implemented code to make the Transfer component more user friendly.

--- src/token_frontend/src/components/Transfer.jsx
+++ src/token_frontend/src/components/Transfer.jsx
@@ -17,8 +17,20 @@ function Transfer() {
   // `setAmount` is the function used to update it
   const [amount, setAmount] = useState("");
 
+  // varaible that determines whether or not the "Transfer" button will be disabled
+  const [isDisabled, setDisabled] = useState(false);
+
+  // variable that stores the message returned from the transfer() function
+  const [feedback, setFeedback] = useState("");
+
+  // variable that determines whether or not the feedback paragraph should be hidden
+  const [isHidden, setIsHidden] = useState(true);
+
   // This asynchronous function is called when the user clicks the "Transfer" button
   async function handleClick() {
+    // Disbale the "Transfer" button once it has been clicked so that a user can't
+    // click it again while a transfer is happening.
+    setDisabled(true);
     // transfer() in main.mo expects a Principal for the first parameter; not a Text
     // so we must convert the recipientId from a String to a Principal.
 
@@ -30,7 +42,16 @@ function Transfer() {
 
     // Call the `transfer` function on the backend smart contract
     // This sends `amountToTransfer` tokens to the `recipient` Principal
-    await token_backend.transfer(recipient, amountToTransfer);
+    const result = await token_backend.transfer(recipient, amountToTransfer);
+
+    // transfer returns "Success" or "Insufficient Funds"
+    setFeedback(result);
+    
+    // only display message once we've recieved the feedback
+    setIsHidden(false);
+
+    // enable the "Transfer" button once the transfer has completed
+    setDisabled(false);
   }
 
   // Return the HTML structure (JSX) that will be displayed in the browser
@@ -71,10 +92,11 @@ function Transfer() {
 
         {/* Transfer button that triggers handleClick when clicked */}
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick}>
+          <button id="btn-transfer" onClick={handleClick} disabled={isDisabled}>
             Transfer
           </button>
         </p>
+        <p hidden={isHidden}>{feedback}</p>
       </div>
     </div>
   );
